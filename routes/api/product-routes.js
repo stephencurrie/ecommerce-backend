89be4624cb87dd61285ebec9c07884b83e4d426c
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-05-28 13:36:36 -0400

    updated product-route.js to get all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,10 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    try {
+      const productData = await Product.findAll();
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   // find all products
   // be sure to include its associated Category and Tag data
-});
+
 
 // get one product
 router.get('/:id', (req, res) => {
