e2a1b6cc47547abca9ef2f3c287ebde327f27628
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-05-29 17:07:33 -0400

    added create route in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,6 +44,14 @@ router.get("/:id", async (req, res) => {
 //   // create a new category
 // });
 
+router.post('/', async (req, res) => {
+    try {
+      const categoryData = await Category.create(req.body.category_name);
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 // update a category by its `id` value
 
